e3d55081769b2f203fa3ab28fd9eaacb9d7284d0
priyamenezesnic <121418678+pmenezes3094@users.noreply.github.com>
2024-03-30 16:32:09 -0700

    Add all gsap animations

--- script.js
+++ script.js
@@ -1,12 +1,12 @@
+// Register ScrollTrigger plugin
 gsap.registerPlugin(ScrollTrigger);
 
-//Loading animations
+// Timeline for loading animations
 let tl = gsap.timeline();
-tl.from(".header", { duration: 1, y: "-200%", ease: "back" });
-tl.from(".hero, main", { duration: 1, opacity: 0, ease: "slow" });
+tl.from("body", { duration: 0.5, opacity: 0.5, ease: "slow"});
+tl.from(".header", { duration: 0.5, y: "-200%", ease: "back" });
 
-
-//scale hero image on scroll
+// Scale hero image on scroll
 gsap.to(".hero-img", {
     scale: 2,
     scrollTrigger: {
@@ -14,35 +14,107 @@ gsap.to(".hero-img", {
       start: "top top",
       end: "bottom top",
       scrub: true,
-      marker: true,
     },
-  });
+});
 
-//fade hero image and content on scroll
-  gsap.to(".hero-content, .hero-img", {
+// Fade hero image and content on scroll
+gsap.to(".hero-content, .hero-img", {
     opacity: 0,
     scrollTrigger: {
       trigger: ".hero",
       start: "top top",
       end: "bottom top",
       scrub: true,
-      marker: true,
     },
-  });
-
-//slide main over hero (Parallax effect)
-gsap.to("main", {
-  y: "-700px",
-  zIndex: 2,
-  scrollTrigger: {
-    trigger: ".hero",
-    start: "top top",
-    end: "bottom top",
-    scrub: true,
-    marker: true,
-  },
 });
 
+// Move hero image and content down as it scrolls up
+gsap.to(".hero .hero-content", {
+    y: 300,
+    zIndex: 0,
+    scrollTrigger: {
+      trigger: ".hero",
+      start: "top top",
+      end: "bottom top",
+      scrub: true,
+    },
+});
+
+// Scale up contact details on trigger
+gsap.from(".contact-details-content", {
+    scale: 0,
+    duration: 1,
+    scrollTrigger: {
+      trigger: ".contact",
+      start: "top bottom",
+    },
+});
+
+// Bounce appear footer details on trigger
+gsap.from("footer", {
+    y: 200,
+    duration: 1,
+    ease: "power",
+    delay: 1,
+    scrollTrigger: {
+      trigger: ".contact",
+      start: "bottom bottom"
+    },
+});
+
+// Scale up bio quote content on trigger
+gsap.from(".bio-quote-content", {
+    scale: 0,
+    duration: 1,
+    scrollTrigger: {
+      trigger: ".bio-quote",
+      start: "top bottom",
+      end: "center center",
+      scrub: 4
+    },
+});
+
+// Reveal text content on scroll
+const textContent = document.querySelectorAll(".text");
+textContent.forEach((element) => {
+    gsap.from(element, {
+        opacity: -1,
+        scrollTrigger: {
+            trigger: element,
+            start: "top bottom",
+            end: "center center",
+            scrub: 4
+        },
+    });
+});
+
+// Reveal card content on scroll
+const cards = document.querySelectorAll(".card-content");
+cards.forEach((element) => {
+    gsap.from(element, {
+        y: 200,
+        scrollTrigger: {
+            trigger: element,
+            start: "top bottom",
+            end: "top 400%",
+            scrub: 4
+        },
+    });
+});
+
+// Reveal case study images on scroll
+const caseStudyImages = document.querySelectorAll(".image");
+caseStudyImages.forEach((element) => {
+    gsap.from(element, {
+        opacity: -1,
+        scrollTrigger: {
+            trigger: element,
+            start: "top bottom",
+            end: "center center",
+            scrub: 4
+        },
+    });
+});
 
 // Smooth Scroll
 const lenis = new Lenis();
