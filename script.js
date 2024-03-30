aa9213021695b92d77d1255c1466697045a6162a
priyamenezesnic <121418678+pmenezes3094@users.noreply.github.com>
2024-03-29 21:50:16 -0700

    insert gsap cdn and smooth scroll library

---
+++ script.js
@@ -0,0 +1,15 @@
+//Smooth Scroll
+const lenis = new Lenis()
+
+lenis.on('scroll', (e) => {
+  console.log(e)
+})
+
+function raf(time) {
+  lenis.raf(time)
+  requestAnimationFrame(raf)
+}
+
+requestAnimationFrame(raf)
+
+
